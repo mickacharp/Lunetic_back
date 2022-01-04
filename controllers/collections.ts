910253a96cde084973ff6c6582593f052ebe2335
Christelle LEGROS <christelle.legros80@sfr.fr>
2022-01-04 20:32:20 +0100

    Start collections routes

--- controllers/collections.ts
+++ controllers/collections.ts
@@ -5,11 +5,6 @@ import * as Collection from '../models/collection';
 import { ErrorHandler } from '../helpers/errors';
 
 collectionsRouter.get('/', (req: Request, res: Response) => {
-    Collection.getAllCollections()
-      .then((collections: Array<ICollection>) => {
-        res.status(200).json(collections);
-
-collectionsRouter.get('/', (req: Request, res: Response) => {
   Collection.getAllCollections()
     .then((collections: Array<ICollection>) => {
       res.status(200).json(collections);
@@ -29,5 +24,23 @@ collectionsRouter.get(
       .catch((err) => next(err));
   }
 );
+
+collectionsRouter.delete('/:id_collection', (req: Request, res: Response) => {
+  const { id_collection } = req.params;
+  Collection.deleteCollection(Number(id_collection))
+    .then((deletedCollection) => {
+      if (deletedCollection) {
+        res
+          .status(200)
+          .send('delete collection for id_collection ' + id_collection);
+      } else {
+        res.status(401).send('No collection found');
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      throw new ErrorHandler(500, 'Collection cannot be updated');
+    });
+});
 
 export default collectionsRouter;
--- models/collection.ts
+++ models/collection.ts
@@ -1,10 +1,8 @@
 import connection from '../db-config';
 import Joi from 'joi';
-import argon2, { Options } from 'argon2';
 import { ResultSetHeader } from 'mysql2';
 import { NextFunction, Request, Response } from 'express';
 import { ErrorHandler } from '../helpers/errors';
-import { builtinModules } from 'module';
 import ICollection from '../interfaces/ICollection';
 
 const getAllCollections = (): Promise<ICollection[]> => {
@@ -23,4 +21,37 @@ const getCollectionById = (idCollection: number): Promise<ICollection> => {
     .then(([results]) => results[0]);
 };
 
-export { getAllCollections, getCollectionById };
+const deleteCollection = (id_collection: number): Promise<boolean> => {
+  return connection
+    .promise()
+    .query<ResultSetHeader>('DELETE FROM collections WHERE id_collection = ?', [
+      id_collection,
+    ])
+    .then(([results]) => results.affectedRows === 1);
+};
+
+const validateCollection = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let required: Joi.PresenceMode = 'optional';
+  if (req.method === 'POST') {
+    required = 'required';
+  }
+  const errors = Joi.object({
+    name: Joi.string().max(255).presence(required),
+  }).validate(req.body, { abortEarly: false }).error;
+  if (error) {
+    next(new ErrorHandler(422, error.message));
+  } else {
+    next();
+  }
+};
+
+export {
+  getAllCollections,
+  getCollectionById,
+  deleteCollection,
+  validateCollection,
+};
