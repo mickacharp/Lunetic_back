f6d2acec30268d6745a8eff5c26350295e41e01e
PatxiEH <patxi.hiribarren@gmail.com>
2022-01-05 20:45:09 +0100

    days and models routes are made

--- controllers/days.ts
+++ controllers/days.ts
@@ -2,9 +2,13 @@ import { Request, Response, Router } from 'express';
 const daysRouter = Router();
 import * as Day from '../models/day';
 
+///////////// GETALL ///////////////
+
 daysRouter.get('/', (req: Request, res: Response) => {
   res.send('days');
 });
+
+///////////// GETONE ///////////////
 
 daysRouter.get('/:id_day', (req: Request, res: Response) => {
   Day.getById(Number(req.params.id_day)).then((day) => {
