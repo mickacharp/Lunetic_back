b0cc139cf34db2908bda033b31c6cc1a4c65fe2c
matthieudubo <90094953+matthieudubo@users.noreply.github.com>
2022-01-05 17:42:18 +0100

    Revert "Days and models routes"

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,11 +1,9 @@
 import * as express from 'express';
-import IOpticianInfo from '../interfaces/IOpticianInfo';
-import INewsInfo from '../interfaces/INewsInfo';
-
+import IUserInfo from '../interfaces/IUserInfo';
 declare global {
   namespace Express {
     interface Request {
-      opticianInfo?: IOpticianInfo;
+      userInfo?: IUserInfo;
     }
   }
 }
--- controllers/auth.ts
+++ controllers/auth.ts
@@ -1,31 +1,29 @@
-import { Request, Response, NextFunction, Router } from 'express';
-import * as Optician from '../models/optician';
-import IOptician from '../interfaces/IOptician';
+import { Request, Response, NextFunction } from 'express';
+const authRouter = require('express').Router();
+import * as User from '../models/user';
+import IUser from '../interfaces/IUser';
 import { ErrorHandler } from '../helpers/errors';
+
 import { calculateToken } from '../helpers/auth';
 
-const authRouter = Router();
-
 authRouter.post(
-  '/', (req: Request, res: Response, next: NextFunction) => {
+  '/',
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { email, password } = req.body as IOptician;
-      Optician.getByEmail(email)
-      .then(async (optician) => {
-        if (!optician) throw new ErrorHandler(401, 'This optician does not exist');
+      const { email, password } = req.body;
+      const user: IUser = await User.getByEmail(email);
+      if (!user) throw new ErrorHandler(401, 'This user does not exist');
       else {
-        const passwordIsCorrect: boolean = await Optician.verifyPassword(
+        const passwordIsCorrect: boolean = await User.verifyPassword(
           password,
-          optician.password
+          user.password
         );
         if (passwordIsCorrect) {
-          const token: string = calculateToken(email, Number(optician.id_optician));
-          res.cookie('optician_token', token);
+          const token = calculateToken(email, Number(user.id_user), user.admin);
+          res.cookie('user_token', token);
           res.send();
         } else throw new ErrorHandler(401, 'Invalid Credentials');
       }
-      })
-      .catch((err) => next(err));
     } catch (err) {
       next(err);
     }
