60e053868cbc80bb5bac2402295dbe14193c2e70
mickacharp <mickaelcharp@gmail.com>
2022-01-10 10:47:06 +0100

    adding cors and @types/cors to connect back-end to front-end

--- index.ts
+++ index.ts
@@ -3,9 +3,18 @@ import cookieParser from 'cookie-parser';
 import 'dotenv/config';
 import { handleError } from './helpers/errors';
 import setupRoutes from './controllers';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const corsOptions: cors.CorsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+};
+
+// middleware cors
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
